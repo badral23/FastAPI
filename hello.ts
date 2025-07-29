0cfee2c9694a7a4e97b7f36e196381834a6532ae
badral23 <dev.badral@gmail.com>
2025-07-29 13:13:59 +0800

    test

---
+++ hello.ts
@@ -0,0 +1,14 @@
+{
+  "wallet_address": "0x93F11C9DC840B017704BFA10Dc8f2d2089eeB4D1",
+  "signed_message": "0x2e5ca2060aa2f1b0dc94878bc090b968d07d2e1719339d6de605fe5abb2c7246585f39fc908414c129e24de2bc298a5adb8eac7cfa854232998ab1b1d080d0111c",
+  "message": "localhost:3000 wants you to sign in with your Ethereum account:\n" +
+    "0x93F11C9DC840B017704BFA10Dc8f2d2089eeB4D1\n" +
+    "\n" +
+    "Please sign with your account\n" +
+    "\n" +
+    "URI: http://localhost:3000\n" +
+    "Version: 1\n" +
+    "Chain ID: 33139\n" +
+    "Nonce: 1b9388d2709c4ee28c9579c686cbd147df733a353a33329794dba2fa6779b730\n" +
+    "Issued At: 2025-07-26T05:03:06.344Z"
+}
